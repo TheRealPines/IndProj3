5002966d4ed5e708f3f9caa368579023845b04e1
Glitch (destiny-tiny-clematis) <none>
2021-02-22 03:31:38 +0000

    🍇🏜 Checkpoint ./public/products.html:27683896/740 ./public/styles.js:27683896/1204

---
+++ public/styles.js
@@ -0,0 +1,25 @@
+function calcTotal(){
+  
+  //initializing itemTotal to 0
+  var itemTotal = 0;
+  
+  //storing the checkbox element
+  var ankle = document.getElementById("ankweights");
+  var dumbbell = document.getElementById("dumbbell");
+  var barbell = document.getElementById("barbell");
+  var vest = document.getElementById("vest");
+  
+  //adding up the selected items
+  (ankle.checked) ?(itemTotal += 39.99) : (itemTotal += 0);
+  (dumbbell.checked) ?(itemTotal += 199.99) : (itemTotal += 0);
+  (barbell.checked) ?(itemTotal += 167.78) : (itemTotal += 0);
+  (vest.checked) ?(itemTotal += 119.99) : (itemTotal += 0);
+  
+  //now calculating the real total with tax and everthing
+  var taxRate = 1.06;
+  var total = itemTotal * taxRate;
+  alert ("Your order total is $" + orderTotal.toFixed(2)); //the toFixed(2 makes it so that there are only two decimal places)
+  
+}
+
+document.getElementById("submit").addEventListener("click", calcTotal);
